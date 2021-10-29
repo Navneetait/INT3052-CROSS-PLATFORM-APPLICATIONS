adfd200bed14fffd7e7609546518063299dadc1d
Navneetait <93364386+Navneetait@users.noreply.github.com>
2021-10-29 15:35:25 +1100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import AppLoading from "expo-app-loading";
+
+import * as Font from "expo-font";
+import Login from "./src/screens/Login";
+import Main from "./src/screens/Main";
+
+const DrawerNavigation = createDrawerNavigator({ Login: Login, Main: Main });
+
+const StackNavigation = createStackNavigator(
+  {
+    DrawerNavigation: {
+      screen: DrawerNavigation
+    },
+    Login: Login,
+    Main: Main
+  },
+  {
+    headerMode: "none"
+  }
+);
+
+const AppContainer = createAppContainer(StackNavigation);
+
+function App() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  if (!isLoadingComplete) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return isLoadingComplete ? <AppContainer /> : <AppLoading />;
+  }
+}
+async function loadResourcesAsync() {
+  await Promise.all([
+    Font.loadAsync({
+      "roboto-regular": require("./src/assets/fonts/roboto-regular.ttf")
+    })
+  ]);
+}
+function handleLoadingError(error) {
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+export default App;
